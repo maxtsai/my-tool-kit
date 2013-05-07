73039b79e3c3f8633c8fd4296abcdec5b611fd06
Max Tsai <haiching.tsai@gmail.com>
2013-05-08 00:30:12 +0800

    bug fixes of collect.

--- collect.py
+++ collect.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+#-*- coding:utf-8 -*-
+
 '''
 Max Tsai <haiching.tsai@gmail.com>
 2013/5/6
@@ -16,7 +18,7 @@ dst_root = ''
 total = 0
 
 def get_cfiletime(filename):
-	return datetime.datetime.fromtimestamp(os.path.getctime(filename)).strftime("%Y-%m-%d")
+	return datetime.datetime.fromtimestamp(os.path.getmtime(filename)).strftime("%Y-%m-%d")
 
 def move_jpeg(filename):
 	global dst_root
@@ -24,7 +26,7 @@ def move_jpeg(filename):
 
 	cmd = "exif -t 0x9003 " + filename
 	stat, log = commands.getstatusoutput(cmd)
-	if log.find('EXIF tags in') != -1:
+	if log.find("'DateTimeOriginal') exists in IFD") != -1:
 		log_list = log.split('\n')
 		log_list = log_list[5].split(' ');
 		if len(log_list) == 0:
@@ -33,17 +35,29 @@ def move_jpeg(filename):
 			exif_date = log_list[3].replace(':', '-')
 	else:
 		exif_date = get_cfiletime(filename)
-	dst_path = dst_root + '/' + exif_date
-	if not os.path.exists(dst_path):
-		os.mkdir(dst_path)
+
+	year = exif_date.split('-')[0]
+	month = exif_date.split('-')[1]
+	date = exif_date.split('-')[2]
+
+	if not os.path.exists(dst_root + '/' + year):
+		os.mkdir(dst_root + '/' + year)
+	if not os.path.exists(dst_root + '/' + year + '/' + month):
+		os.mkdir(dst_root + '/' + year + '/' + month)
+	if not os.path.exists(dst_root + '/' + year + '/' + month + '/' + date):
+		os.mkdir(dst_root + '/' + year + '/' + month + '/' + date)
+
+	dst_path = dst_root + '/' + year + '/' + month + '/' + date
 	head, tail = os.path.split(filename)
 	if not os.path.exists(dst_path + '/' + tail):
 		total += 1
 		shutil.move(filename, dst_path)
+		#print dst_path + ' <= ' + filename
 	else:
-		for i in range(5):
+		for i in range(50):
 			if not os.path.exists(dst_path + '/' + str(i) + '_' + tail):
 				shutil.move(filename, dst_path + '/' + str(i) + '_' + tail)
+				#print dst_path + '/' + str(i) + '_' + tail + ' <= ' + filename
 				total += 1
 				break
 
@@ -51,18 +65,30 @@ def move_other_type(filename):
 	global dst_root
 	global total
 	_date = get_cfiletime(filename)
-	dst_path = dst_root + '/' + _date
 
-	if not os.path.exists(dst_path):
-		os.mkdir(dst_path)
+	year = _date.split('-')[0]
+	month = _date.split('-')[1]
+	date = _date.split('-')[2]
+
+	if not os.path.exists(dst_root + '/' + year):
+		os.mkdir(dst_root + '/' + year)
+	if not os.path.exists(dst_root + '/' + year + '/' + month):
+		os.mkdir(dst_root + '/' + year + '/' + month)
+	if not os.path.exists(dst_root + '/' + year + '/' + month + '/' + date):
+		os.mkdir(dst_root + '/' + year + '/' + month + '/' + date)
+
+	dst_path = dst_root + '/' + year + '/' + month + '/' + date
+
 	head, tail = os.path.split(filename)
 	if not os.path.exists(dst_path + '/' + tail):
 		total += 1
 		shutil.move(filename, dst_path)
+		#print dst_path + ' <= ' + filename
 	else:
-		for i in range(5):
+		for i in range(50):
 			if not os.path.exists(dst_path + '/' + str(i) + '_' + tail):
 				shutil.move(filename, dst_path + '/' + str(i) + '_' + tail)
+				#print dst_path + '/' + str(i) + '_' + tail + ' <= ' + filename
 				total += 1
 				break
 
@@ -74,6 +100,10 @@ def walk_func(cur_dir, direction, files):
 			sys.stdout.flush()
 			move_jpeg(direction + '/' + i)
 		if fileExtension == '.mov' or fileExtension == '.MOV':
+			sys.stdout.write("x")
+			sys.stdout.flush()
+			move_other_type(direction + '/' + i)
+		if fileExtension == '.tif' or fileExtension == '.TIF':
 			sys.stdout.write("x")
 			sys.stdout.flush()
 			move_other_type(direction + '/' + i)
@@ -96,3 +126,5 @@ if not os.path.exists(dst_root):
 os.path.walk("./", walk_func, "./")
 sys.stdout.write('\n')
 print 'total = ' + str(total)
+
+
